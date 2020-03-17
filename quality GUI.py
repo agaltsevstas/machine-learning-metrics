8b72d02bd4ac3350b64c9dcaac4edc28f7ca0a56
stas.agaltsev <sosimba-sosimbovich@mail.ru>
2020-03-17 11:21:10 +0300

    Новые комментарии 2

--- quality GUI.py
+++ quality GUI.py
@@ -13,6 +13,7 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 import matplotlib.pyplot as plt
 import pm_utils as pm
+from sklearn.metrics import precision_recall_fscore_support, accuracy_score
 
 # Метрики для обнаружения объектов
 class Metrics:
@@ -39,23 +40,28 @@ class Metrics:
             return 0
         return self.tp / sum
 
-    # Отзыв
+    # Полнота
     def recall(self):
         sum = self.tp + self.fn
         if sum == 0:
             return 0
         return self.tp / sum
 
+    # Гармоническое среднее между точностью и полнотой
+    def f1_score(self):
+        return 2 / ((1 / self.precision()) + (1 / self.recall()))
+
+    # Вывод значений
     def values(self):
         num = 1
         if self.number() > 0:
             num = self.number()
-        return 1.0*self.fn/num, self.fp/num, self.accuracy(), self.precision(), self.recall()
+        return 1.0*self.fn/num, 1.0*self.fp/num, self.accuracy(), self.precision(), self.recall(), self.f1_score()
 
     # Название метрик
     @staticmethod
     def str_header():
-        return ["fn", "fp", "acc", "prec", "rec"]
+        return ["fn", "fp", "acc", "prec", "rec", "f1"]
 
 # Вычисление метрик
 def metrics_calculation(xml_paths):
@@ -63,44 +69,44 @@ def metrics_calculation(xml_paths):
     # Считывание xml файлов
     files_objects = [pm.readXml(xml_path, "basename") for xml_path in xml_paths]
     # Параллельный цикл для 2 xml файлов
-    for neuronet_file, labeled_file in zip(files_objects[0], files_objects[1]):
+    for labeled_file, neuronet_file in zip(files_objects[0], files_objects[1]):
         # Проверка на совпадение изображений
-        if labeled_file != neuronet_file:
+        if neuronet_file != labeled_file:
             continue
         # Получение объекта и его координат
-        neuronet_objects = files_objects[0][neuronet_file]
-        labeled_objects = files_objects[1][labeled_file]
+        labeled_objects = files_objects[0][labeled_file]
+        neuronet_objects = files_objects[1][neuronet_file]
         # Если объекты не найдены в одном из xml файлов
-        if any(len(v) == 0 for v in (neuronet_objects, labeled_objects)):
+        if any(len(v) == 0 for v in (labeled_objects, neuronet_objects)):
             continue
-        neuronet_pairs = np.zeros(len(neuronet_objects), np.int32)
         labeled_pairs = np.zeros(len(labeled_objects), np.int32)
+        neuronet_pairs = np.zeros(len(neuronet_objects), np.int32)
         # Проверка на совпадение объектов в xml файлах
-        for i in range(len(neuronet_objects)):
-            neuronet_class = neuronet_objects[i][4]
-            if neuronet_class not in metrics:
-                metrics[neuronet_class] = Metrics()
-            for j in range(len(labeled_objects)):
-                labeled_class = labeled_objects[j][4]
+        for i in range(len(labeled_objects)):
+            labeled_class = labeled_objects[i][4]
+            if labeled_class not in metrics:
+                metrics[labeled_class] = Metrics()
+            for j in range(len(neuronet_objects)):
+                neuronet_class = neuronet_objects[j][4]
                 # True positive
-                if neuronet_class == labeled_class and pm.intersection_over_union(neuronet_objects[i],
-                                                                                  labeled_objects[j],
+                if labeled_class == neuronet_class and pm.intersection_over_union(labeled_objects[i],
+                                                                                  neuronet_objects[j],
                                                                                   0.5):
-                    neuronet_pairs[i] = 1
-                    labeled_pairs[j] = 1
-                    metrics[neuronet_class].tp += 1
+                    labeled_pairs[i] = 1
+                    neuronet_pairs[j] = 1
+                    metrics[labeled_class].tp += 1
         # False positive
-        for j in range(len(labeled_objects)):
-            if labeled_pairs[j] == 0:
-                labeled_class = labeled_objects[j][4]
-                if labeled_class not in metrics:
-                    metrics[labeled_class] = Metrics()
-                metrics[labeled_class].fp += 1
-        # False negative
         for j in range(len(neuronet_objects)):
             if neuronet_pairs[j] == 0:
                 neuronet_class = neuronet_objects[j][4]
-                metrics[neuronet_class].fn += 1
+                if neuronet_class not in metrics:
+                    metrics[neuronet_class] = Metrics()
+                metrics[neuronet_class].fp += 1
+        # False negative
+        for j in range(len(labeled_objects)):
+            if labeled_pairs[j] == 0:
+                labeled_class = labeled_objects[j][4]
+                metrics[labeled_class].fn += 1
     return Metrics.str_header(), metrics
 
 # GUI приложение
@@ -117,10 +123,10 @@ class Application(tk.Frame):
         self.label_second.grid(row=1, column=1, sticky="nsew")
         self.list_box = tk.Listbox(selectmode=tk.EXTENDED, height=2)
         self.list_box.grid(row=2, columnspan=3, sticky="nsew")
-        self.button_file_first = tk.Button(text="Load XML neuronet",   activeforeground="blue", bg="#4e9a06",
+        self.button_file_first = tk.Button(text="Load XML labeled",   activeforeground="blue", bg="#4e9a06",
                                            command=lambda: self.open_xml(self.file_first))
         self.button_file_first.grid(row=0, column=0, sticky="nsew")
-        self.button_file_second = tk.Button(text="Load XML labeled data",  activeforeground="blue", bg="#ffff00",
+        self.button_file_second = tk.Button(text="Load XML neuronet data",  activeforeground="blue", bg="#ffff00",
                                             command=lambda: self.open_xml(self.file_second))
         self.button_file_second.grid(row=1, column=0, sticky="nsew")
         self.button_histogram = tk.Button(text="histogram",  activeforeground="blue", bg="#ffb841",
@@ -141,9 +147,9 @@ class Application(tk.Frame):
         # Проверка пути к xml файла на пустоту и на совпадение ранее загруженного xml файла с таким же именем
         if len(filepath) and not filepath == self.filepath_first and not filepath == self.filepath_second:
             """
-            Если загружается xml neuronet файл, то он добавляется в начало списка и удаляется ранее добавленный
+            Если загружается xml labeled файл, то он добавляется в начало списка и удаляется ранее добавленный
             файл.
-            Если загружается xml labeled файл, то он добавляется в конец списка и удаляется ранее добавленный
+            Если загружается xml neuronet файл, то он добавляется в конец списка и удаляется ранее добавленный
             файл.
             """
             if file_selection == self.file_first:
@@ -175,7 +181,9 @@ class Application(tk.Frame):
                      "fp — false positive\n"
                      "acc — accuracy\n"
                      "prec — precision\n"
-                     "rec — recall", x=0.95, y=0.5)
+                     "rec — recall\n"
+                     "f1 — score",
+                     x=0.95, y=0.5)
         # Вывод вычисленных метрик по данным из xml файлов в консоль и на гистограмму
         for i, classname in enumerate(metrics):
             print("%10s\t" % classname + "\t".join("{:0.2f}".format(value) for value in metrics[classname].values()))
@@ -194,7 +202,7 @@ class Application(tk.Frame):
         # Спрятать интервал между гистограммами
         plt.subplots_adjust(wspace=0, hspace=0)
         plt.show()
-        self.tk.quit()
+        self.quit()
 
     # Удаление файла(ов) при нажатии или выделении через shift
     def delete_file(self):
@@ -209,11 +217,11 @@ class Application(tk.Frame):
             self.list_box.delete(i)
             print(filepath + " DELETED")
 
-    """
-    Таймер, который проверяет через каждые (несколько) миллисекунд на наличие двух загруженных xml файлов.
-    Если оба xml файла не загружены, то кнопка button_histogram недоступна.
-    """
     def update_clock(self):
+        """
+        Таймер, который проверяет через каждые (несколько) миллисекунд на наличие двух загруженных xml файлов.
+        Если оба xml файла не загружены, то кнопка button_histogram недоступна.
+        """
         if self.filepath_first:
             self.label_first.config(text=os.path.basename(self.filepath_first) + " LOADED", fg="blue")
         else:
